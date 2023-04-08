14921e290a5c95d2980f6aa3928b501d5d0cd308
evlawless <ev.lawless@gmail.com>
2023-04-07 23:22:20 -0400

    A couple minor style changes before bed :)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,20 +2,19 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
 
 import { api } from "~/utils/api";
 
 const TarotLink = ({title}:{title:string}) => {
 	return (
-		<Link href="#" className="">
-		<div className="w-36 h-72 hover:w-40 hover:h-80  p-1 bg-amber-100 hover:shadow-xl hover:shadow-emerald-500">
+		<Link href="#" className="hover:animate-pulse">
+		<div className="w-36 h-72 p-1 bg-amber-100 hover:shadow-xl hover:shadow-emerald-500">
 			<div className="flex flex-col-reverse border border-black w-full h-full bg-gradient-to-br from-emerald-950 to-slate-950">
 				<div className="w-full h-8 flex justify-center bg-amber-100 font-tarot text-xl">
 				{title}
 				</div>
 				<div className="w-full h-full overflow-hidden">
-					<Image alt="a random image" src={"https://picsum.photos/300/600"} width={300} height={600}/>
+					<Image alt="a random image" src={"https://picsum.photos/100/200"} width={300} height={600}/>
 				</div>
 			</div>
 		</div>
@@ -34,7 +33,7 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 				
 			</Head>
-			<main className="flex min-h-screen flex-col items-center justify-center bg-rose-950">
+			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-rose-950 to-black">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 					<h1 className="text-5xl font-extrabold tracking-tight text-rose-200 sm:text-[5rem]">
 						Create <span className="text-rose-300">HAUNTED</span> App
