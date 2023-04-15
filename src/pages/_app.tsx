05e1998c5fb50e8836c591c87ef71ca86f4038ec
evlawless <ev.lawless@gmail.com>
2023-04-15 17:17:45 -0400

    Messing around with Framer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from '@clerk/nextjs';
 
 import { api } from "~/utils/api";
+
 
 import "~/styles/globals.css";
 
@@ -8,14 +10,18 @@ import "~/styles/globals.css";
 import { Josefin_Sans } from "next/font/google"
 
 const josefinSans = Josefin_Sans({
-  weight:"300",
-  subsets:['latin'],
-  variable:'--font-josefinSans'
+	weight: "300",
+	subsets: ['latin'],
+	variable: '--font-josefinSans'
 })
 
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+	return (
+		<ClerkProvider {...pageProps}>
+			<Component {...pageProps} />
+		</ClerkProvider>
+	);
 };
 
 export default api.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,56 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-import { api } from "~/utils/api";
+import { dark } from "@clerk/themes";
 
-const TarotLink = ({title, img, alt}:{title:string, img: string, alt:string}) => {
+import { useAuth, SignedIn, SignedOut, SignIn, SignOutButton } from "@clerk/nextjs";
+
+import { motion } from "framer-motion";
+
+import { api } from "~/utils/api";
+import { useState } from "react";
+
+const TarotLink = ({ title, img, alt, onClick, href = "#" }: { title: string, img: string, alt: string, onClick?: () => void, href?: string }) => {
 	return (
-		<Link href="#" className="hover:animate-pulse">
-		<div className="w-36 h-72 p-1 bg-amber-100 hover:shadow-xl hover:shadow-emerald-500">
-			<div className="flex flex-col-reverse border border-black w-full h-full bg-gradient-to-br from-emerald-950 to-slate-950">
-				<div className="w-full h-8 flex justify-center bg-amber-100 font-tarot text-xl">
-				{title}
-				</div>
-				<div className="w-full h-full overflow-hidden">
-					<Image alt={alt} src={img} width={300} height={600}/>
+		<Link href={href} className="hover:animate-pulse" onClick={onClick}>
+			<div className="w-36 h-72 p-1 bg-amber-100 hover:shadow-xl hover:shadow-emerald-500">
+				<div className="flex flex-col-reverse border border-black w-full h-full bg-gradient-to-br from-emerald-950 to-slate-950">
+					<div className="w-full h-8 flex justify-center bg-amber-100 font-tarot text-xl">
+						{title}
+					</div>
+					<div className="w-full h-full overflow-hidden">
+						<Image alt={alt} src={img} width={300} height={600} />
+					</div>
 				</div>
 			</div>
-		</div>
 		</Link>
+	)
+}
+
+const PactTarotLink = () => {
+	const [focus, setFocus] = useState(false);
+
+	return (
+		<motion.div layout>
+			{
+				focus ?
+					(<div className="bg-gradient-to-r from-teal-600 to-purple-950">
+						<SignedIn>
+							<div>
+								<SignOutButton />
+							</div>
+						</SignedIn>
+						<SignedOut>
+							<SignIn
+								appearance={{
+									baseTheme: dark
+								}}
+							/>
+						</SignedOut>
+					</div>) :
+					<TarotLink title="SELL your SOUL" img="/assets/tools.jpg" alt="A Dark Pact (login)" onClick={() => { setFocus((f) => !f) }} />
+			}
+		</motion.div>
 	)
 }
 
@@ -27,7 +61,7 @@ const PentagramSpinner = () => {
 		<div className="flex items-center justify-center w-full h-full">
 			<div className="animate-ping rounded-full h-32 w-32 border-t-2 border-b-2 border-emerald-500 flex items-center justify-center">
 				{/* star svg lmaooo */}
-				<Image src="/assets/pentagram.svg" width={100} height={100} alt="pentagram"/>
+				<Image src="/assets/pentagram.svg" width={100} height={100} alt="pentagram" />
 			</div>
 		</div>
 	)
@@ -35,7 +69,7 @@ const PentagramSpinner = () => {
 
 
 const Home: NextPage = () => {
-	const hello = api.example.hello.useQuery({ text: "from tRPC" });
+	// const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
 	return (
 		<>
@@ -43,23 +77,23 @@ const Home: NextPage = () => {
 				<title>Create Haunted App</title>
 				<meta name="description" content="A haunted website built with the T3 Stack" />
 				<link rel="icon" href="/favicon.ico" />
-				
+
 			</Head>
 			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-rose-950 to-black">
 				<div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
 					<h1 className="text-5xl font-extrabold tracking-tight text-rose-200 sm:text-[5rem]">
 						Create <span className="text-rose-300">HAUNTED</span> App
 					</h1>
-					<PentagramSpinner />
+					{/* <PentagramSpinner /> */}
 					<div className=" w-full flex place-content-around h-full">
 						<TarotLink alt="tools" img="/assets/city.jpg" title="FIRST PAGE" />
 						<TarotLink alt="" img="/assets/forks.jpg" title="SECOND PAGE" />
-						<TarotLink alt="" img="/assets/pier.jpg" title="THIRD PAGE" />
+						<PactTarotLink />
 						<TarotLink alt="" img="/assets/trees.jpg" title="FOURTH PAGE" />
-						<TarotLink alt="" img="/assets/tools.jpg" title="FIFTH PAGE" />
+						<TarotLink alt="" img="/assets/tools.jpg" title="ONE of GHOST" />
 					</div>
 					{/* <p className="text-2xl text-rose-100">
-						{hello.data ? hello.data.greeting : "Loading tRPC query..."}
+						{hello.data ? hello.data.greeting : "Loading tRPC query... (figure out a thing for this)"}
 					</p> */}
 				</div>
 			</main>
